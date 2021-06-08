ee86e5a1f482ab4804c4e06922445fb5344ec249
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-06-08 10:16:02 -0300

    added folders

---
+++ SampleApps/Await/Await/AwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwaitApp.swift
+//  Await
+//
+//  Created by Gabriel Ferreira de Carvalho on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Router()
+        }
+    }
+}
---
+++ SampleApps/Await/Await/Router.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Await
+//
+//  Created by Gabriel Ferreira de Carvalho on 08/06/21.
+//
+
+import SwiftUI
+
+struct Router: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Router()
+    }
+}
